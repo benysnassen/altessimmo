a964c1059aaa5e5f2612363acd556ccaea499f40
benysnassen <altessimmo@gmail.com>
2025-10-16 22:02:33 +0100

    ajout ville hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -80,7 +80,7 @@ export default function Hero() {
             transition={{ duration: 1, delay: 2 }}
             className="text-lg font-light mb-16 text-white/60 tracking-widest uppercase"
           >
-            Tétouan • Martil • M'diq • Cabo Negro
+            Tétouan • Martil • M&aposdiq • Cabo Negro
           </motion.p>
         </motion.div>
         
