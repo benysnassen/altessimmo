0ad08e3f3527676361f294b1c3be87c235486db5
benysnassen <altessimmo@gmail.com>
2025-10-16 18:23:58 +0100

    Ajout du script Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/Footer";
+import Script from "next/script"; // Importer le composant Script
 
 export const metadata: Metadata = {
   title: "Altessimmo Tétouan - Propriétés d'exception",
@@ -19,6 +20,19 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+
+        {/* Intégration Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VDNN5GYFNP');
+            `,
+          }}
+        />
       </body>
     </html>
   );
