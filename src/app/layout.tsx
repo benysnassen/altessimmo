1c4bc8e939f4b05a080881b099d31d26f69ccd14
benysnassen <altessimmo@gmail.com>
2025-10-16 18:33:18 +0100

    force redeploy3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/Footer";
-import Script from "next/script"; // Importer le composant Script
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Altessimmo Tétouan - Propriétés d'exception",
@@ -16,24 +16,28 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
-        <main>
-          {children}
-        </main>
-        <Footer />
-
-        {/* Intégration Google Analytics */}
+        {/* Script pour Google Analytics */}
         <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX`} // Remplace par ton ID
+        />
+        <Script
+          id="google-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-VDNN5GYFNP');
+              gtag('config', 'G-VDNN5GYFNP'); 
             `,
           }}
         />
+
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
 }
+
